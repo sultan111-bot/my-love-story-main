f4ea209fdb3e97cbb74969a6afeb844595f6dc0b
Sultan Harahara <sultanharahara1@gmail.com>
2026-06-07 17:44:31 +0700

    Temporarily disable service worker to test

--- src/main.jsx
+++ src/main.jsx
@@ -5,27 +5,27 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(<App />);
 
 // Register service worker untuk PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(reg => {
-        console.log('✅ Service Worker registered:', reg);
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('/service-worker.js')
+//       .then(reg => {
+//         console.log('✅ Service Worker registered:', reg);
         
-        // Check for updates setiap 1 jam
-        setInterval(() => {
-          reg.update();
-        }, 60 * 60 * 1000);
+//         // Check for updates setiap 1 jam
+//         setInterval(() => {
+//           reg.update();
+//         }, 60 * 60 * 1000);
 
-        // Check update ketika user fokus ke tab
-        document.addEventListener('visibilitychange', () => {
-          if (document.visibilityState === 'visible') {
-            reg.update();
-          }
-        });
-      })
-      .catch(err => {
-        console.error('❌ SW registration failed:', err);
-      });
-  });
-}
+//         // Check update ketika user fokus ke tab
+//         document.addEventListener('visibilitychange', () => {
+//           if (document.visibilityState === 'visible') {
+//             reg.update();
+//           }
+//         });
+//       })
+//       .catch(err => {
+//         console.error('❌ SW registration failed:', err);
+//       });
+//   });
+// }
